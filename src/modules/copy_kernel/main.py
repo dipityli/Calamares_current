ddd1475df177f6713f779038de17f56209b998ae
portergos <portergoslinux@gmail.com>
2019-07-18 13:18:29 -0300

    test

---
+++ src/modules/copy_kernel/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import libcalamares
+import subprocess
+from libcalamares.utils import check_target_env_call, target_env_call
+from libcalamares.utils import *
+import os
+
+
+#def run_mkinitcpio():
+#    """ Runs mkinitcpio with given kernel profile """
+#    kernel = libcalamares.job.configuration['kernel']
+#    check_target_env_call(['mkinitcpio', '-P'])
+
+
+def run():
+    """ Calls routine to create kernel initramfs image.
+
+    :return:
+    """
+    root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+    try:
+     subprocess.check_call(["cp", "/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz", root_mount_point + "/boot/vmlinuz-linux"])
+    except:
+     pass # doing nothing on exception
+    try:
+     subprocess.check_call(["cp", "/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-lts", root_mount_point + "/boot/vmlinuz-linux-lts"])
+    except:
+     pass # doing nothing on exception
+
+    return None
