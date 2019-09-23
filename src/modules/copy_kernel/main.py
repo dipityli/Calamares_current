2c4e6cffb1cdb8b654ce67ea5dfc05eea53c953f
Portergos Linux <portergoslinux@gmail.com>
2019-09-23 18:19:23 -0300

    Update main.py

--- src/modules/copy_kernel/main.py
+++ src/modules/copy_kernel/main.py
@@ -24,15 +24,15 @@ def run():
     except:
      pass # doing nothing on exception
 
-    try:
-     os.system("lspci >vbox.txt")
-    except:
-     pass
+    #try:
+    # os.system("lspci >vbox.txt")
+    #except:
+    # pass
 
-    if not 'VirtualBox' in open('vbox.txt').read():
-     try:
-      subprocess.check_call(["pacman", "-Rns", "virtualbox-guest-utils", "virtualbox-guest-modules-arch", "--noconfirm", "--root", root_mount_point])
-     except:
-      pass
+    #if not 'VirtualBox' in open('vbox.txt').read():
+     #try:
+     # subprocess.check_call(["pacman", "-Rns", "virtualbox-guest-utils", "virtualbox-guest-modules-arch", "--noconfirm", "--root", root_mount_point])
+     #except:
+     # pass
 
     return None
