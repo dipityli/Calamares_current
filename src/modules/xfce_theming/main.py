c68365c365011820117257071857caac9e42f9f7
Portergos Linux <portergoslinux@gmail.com>
2019-11-20 20:55:11 -0300

    Update main.py

--- src/modules/xfce_theming/main.py
+++ src/modules/xfce_theming/main.py
@@ -6,28 +6,21 @@
 # Trying K.I.S.S filosophy
 # Detect XFCE4 DE netinstall
 
-#################################################################
-# PROTOTYPE
-#################################################################
-
 import subprocess
 import libcalamares
 from pathlib import Path
 
 root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
 
-
-xfce_installed = Path("root_mount_point + /usr/share/xsessions/xfce.desktop")
-
-    try:
-        if not xfce_installed.exists():
-            subprocess.call(???????.split(' '))
-    except:
-        pass
-
+def run():
+    # Have my doubts if root mount point + path actually work in path function
+    xfce_installed = Path("root_mount_point + /usr/share/xsessions/xfce.desktop")
+    # cleaning stuff from cleaner script may be needed
+    # config copied before creating new user, so use skel folder
     COPY_CMD = "cp -f"
-    DEST_ETC = "/etc"
-
-    subprocess.call(COPY_CMD.split(' ') + [PACMAN_CONF] + [root_mount_point + DEST_ETC])
-
-    subprocess.call(COPY_CMD.split(' ') + ["/tmp/run_once"] + [root_mount_point + "/tmp/run_once"])
+    SKEL_CONFIG = "/etc/skel/.config/xfce"
+        try:
+            if xfce_installed.exists():
+                    subprocess.call(COPY_CMD.split(' ') + [SKEL_CONFIG] + [root_mount_point + SKEL_CONFIG])
+        except:
+            pass
