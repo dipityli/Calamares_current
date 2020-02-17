a407effe56cee009b7e198f3f6acb7b7314cea5b
Portergos Linux <portergoslinux@gmail.com>
2020-02-17 17:17:24 -0300

    Update main.py

--- src/modules/copy_kernel/main.py
+++ src/modules/copy_kernel/main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
 
 import libcalamares
 import subprocess
 from libcalamares.utils import check_target_env_call, target_env_call
 from libcalamares.utils import *
-import os
-
 
 def run():
-    """ Calls routine to create kernel initramfs image.
+    """ Copy kernel and install scripts to target system.
 
     :return:
     """
+
     root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+
+    # Copy kernels
     try:
      subprocess.check_call(["cp", "/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz", root_mount_point + "/boot/vmlinuz-linux"])
     except:
@@ -23,16 +23,14 @@ def run():
      subprocess.check_call(["cp", "/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-lts", root_mount_point + "/boot/vmlinuz-linux-lts"])
     except:
      pass # doing nothing on exception
-
-    #try:
-    # os.system("lspci >vbox.txt")
-    #except:
-    # pass
-
-    #if not 'VirtualBox' in open('vbox.txt').read():
-     #try:
-     # subprocess.check_call(["pacman", "-Rns", "virtualbox-guest-utils", "virtualbox-guest-modules-arch", "--noconfirm", "--root", root_mount_point])
-     #except:
-     # pass
+    # Copy cleaner script for install process
+    try:
+     subprocess.check_call(["cp", "-f", "/usr/bin/cleaner_script.sh", root_mount_point + "/usr/bin"])
+    except:
+     pass # doing nothing on exception
+    try:
+     subprocess.check_call(["cp", "-f", "/usr/bin/chrooted_cleaner_script.sh", root_mount_point + "/usr/bin"])
+    except:
+     pass # doing nothing on exception
 
     return None
