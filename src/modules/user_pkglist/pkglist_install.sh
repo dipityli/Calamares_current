ec436c067f0d2cb553fdb1991e0fe5e4bb1dab1d
Portergos Linux <portergoslinux@gmail.com>
2021-01-19 18:10:01 -0300

    Update pkglist_install.sh

--- src/modules/user_pkglist/pkglist_install.sh
+++ src/modules/user_pkglist/pkglist_install.sh
@@ -5,15 +5,11 @@ _pkglist="/home/liveuser/user_pkglist.txt"
 if [ -f "$_pkglist" ]
 
 then
-    # Need to add module to calamares at some new module
     
-    _pacstrap="/usr/bin/pacstrap_calamares -c"
     _chroot_path=$(cat /tmp/chrootpath.txt)
 
     for pkgs in $(cat $_pkglist)
     do
-        _pacstrap -c $_chroot_path $pkgs
-        # or use pacman - executed from live environment
-        # pacman -S $pkgs --noconfirm --needed --root $_chroot_path
+        pacman -S $pkgs --noconfirm --needed --root $_chroot_path
     done
 fi
