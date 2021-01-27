364ad551bce4a2d8a343e87d0f3d8bbbc14033cf
Portergos Linux <portergoslinux@gmail.com>
2021-01-27 12:19:06 -0300

    Update pkglist_install.sh

--- src/modules/user_pkglist/pkglist_install.sh
+++ src/modules/user_pkglist/pkglist_install.sh
@@ -8,7 +8,7 @@ then
     
     _chroot_path=$(cat /tmp/chrootpath.txt)
 
-    for pkgs in $(cat $_pkglist)
+    for pkgs in $(cat $_pkglist | grep -v '^[ ]*#')
     do
         pacman -Sy $pkgs --noconfirm --needed --root $_chroot_path
     done
